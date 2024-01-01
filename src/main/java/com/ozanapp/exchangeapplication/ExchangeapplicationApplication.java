468a35a598a5bbc9fb1de566123ac5c6f627e71b
Umut Teker <144879487+umuttekernordlb@users.noreply.github.com>
2024-01-01 18:41:54 +0300

    initial commit

---
+++ src/main/java/com/ozanapp/exchangeapplication/ExchangeapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.ozanapp.exchangeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozanapp/exchangeapplication/ExchangeapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozanapp.exchangeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
